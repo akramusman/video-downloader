7a3a29b504264000c243a2801bf3d039aa245258
Usman Akram <akramusmanecv@gmail.com>
2025-08-03 19:39:59 +0500

    Update main.py to use host 0.0.0.0 and dynamic port

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-from flask import Flask, send_from_directory
-from flasgger import Swagger
+from flask import Flask
 from flask_cors import CORS
 from api.routes import api
 import os
 
-app = Flask(__name__, static_folder='static')
-CORS(app)  # <-- Add this line
-swagger = Swagger(app)
+app = Flask(__name__)
+CORS(app)
 app.register_blueprint(api)
-
-@app.route('/')
-def index():
-    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
